2b9086fe9bc02232aa51d05e270939c9808c1a44
gboigwe <geakande@gmail.com>
2023-11-14 14:42:50 +0100

    Task 0

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+ * _printf - The function that writes output to std
+ * @format: To handle formated string
+ *
+ * Return: The counted output
+ */
+int _printf(const char *format, ...)
+{
+	va_list compile;
+	va_start(compile, format);
+
+	int ls = 0;
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			if (*(format + 1) == 'c')
+			{
+				char val = va_arg(compile, int);
+				_putchar(val);
+				ls++;
+			}
+			else if (*(format + 1) == 's')
+			{
+				const char *cha = va_arg(compile, const char *);
+				while (*cha != '\0')
+				{
+					_putchar(*cha);
+					cha++;
+					ls++;
+				}
+			}
+			else if (*(format + 1) == '%')
+			{
+				_putchar('%');
+				ls++;
+			}
+			else
+			{
+				_putchar('%');
+				ls++;
+			}
+			format += 2;
+		}
+		else
+		{
+			_putchar(*format);
+			ls++;
+			format++;
+		}
+	}
+	va_end(compile);
+	return (ls);
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#ifndef HEADER_H
+#define HEADER_H
+
+int _putchar(char c);
+
+#endif
