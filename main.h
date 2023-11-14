fbe3c6e69aba9819af307ef2cb9c05a301fd1a51
gboigwe <geakande@gmail.com>
2023-11-14 15:38:55 +0100

    A task

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
-#include <stdarg.h>
-#include <stdlib.h>
 #ifndef HEADER_H
 #define HEADER_H
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+
 #endif
